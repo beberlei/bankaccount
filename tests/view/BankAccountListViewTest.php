be5887240c0fa257c81ce65c4e0bc8ebc4561a89
Sebastian Bergmann <sb@sebastian-bergmann.de>
2011-02-07 08:44:35 +0100

    Add test for BankAccountListView::render().

---
+++ tests/view/BankAccountListViewTest.php
@@ -0,0 +1,21 @@
+<?php
+class BankAccountListViewTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @covers BankAccountListView::render
+     */
+    public function testIsRenderedCorrectly()
+    {
+        $request = new Request(array('REQUEST_URI' => '/bankaccount/'));
+
+        $response = new Response;
+        $response->set('ids', array(1));
+
+        $view = new BankAccountListView($request, $response);
+
+        $this->assertEquals(
+          '<ul><li><a href="/bankaccount/show/id/1">Bank Account #1</a></li></ul>',
+          $view->render()
+        );
+    }
+}
