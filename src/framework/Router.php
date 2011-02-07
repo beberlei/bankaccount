f96bb84ba052cddc932d1daa744781794d1aa83a
Sebastian Bergmann <sb@sebastian-bergmann.de>
2011-02-07 09:55:36 +0100

    Fix issue reported by PHP_CodeSniffer.

--- src/framework/Router.php
+++ src/framework/Router.php
@@ -31,9 +31,10 @@ class Router extends HashMap
             throw new RuntimeException;
         }
 
-        $keys = array_keys($parts);
+        $keys  = array_keys($parts);
+        $count = count($keys);
 
-        for ($i = 0; $i < count($keys); $i += 2) {
+        for ($i = 0; $i < $count; $i += 2) {
             $request->set($parts[$keys[$i]], $parts[$keys[$i+1]]);
         }
 
